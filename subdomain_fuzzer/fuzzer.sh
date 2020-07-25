ab0ffaa0863dcc981899fc0021d6cb48f988cad2
Ph055a <ph055a@protonmail.com>
2020-07-25 12:42:51 +0800

    simplified

--- subdomain_fuzzer/fuzzer.sh
+++ subdomain_fuzzer/fuzzer.sh
@@ -4,14 +4,6 @@ echo "[*] Enter domain to start subdomain brute forcing"
 echo "[*] Example: hackerone.com"
 read TARGET
 
-SCRAPED_WORDLIST="$HOME/BugBounty/$TARGET/scraped_wordlist.txt"
-KEYS_WORDLIST="$HOME/BugBounty/$TARGET/keys_wordlist.txt"
-PATHS_WORDLIST="$HOME/BugBounty/$TARGET/paths_wordlist.txt"
-PATHS="$HOME/BugBounty/$TARGET/paths.txt"
-FORBIDDEN_URLS="$HOME/BugBounty/$TARGET/forbidden_urls.txt"
-UP_URLS="$HOME/BugBounty/$TARGET/up_urls.txt"
-
-
 file_check(){
     if [ -f $HOME/BugBounty/$TARGET  ]; then
         echo "$TARGET directory already exists" 
@@ -27,89 +19,60 @@ file_check(){
     
 }
 
-pause(){
-    echo "Sleep for a 15 minutes"
-    sleep 15m
-}
-
-custom_wordlist(){
-    gobuster dns -d $TARGET -w $SCRAPED_WORDLIST -o $HOME/BugBounty/$TARGET/subdomains.txt
-}
-
 dns_commonspeak(){
     echo "Brute forcing domains"
-    gobuster dns -d $TARGET -w ~/Lists/commonspeak2-wordlists/subdomains/subdomains.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/commonspeak2-wordlists/subdomains/subdomains.txt -o $HOME/BugBounty/$TARGET/common_speak_results.txt
     echo "Completed Commonspeak"
 }
 
 dns_seclist(){
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Web-Content/directory-list-lowercase-2.3-big.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Web-Content/directory-list-lowercase-2.3-big.txt -o $HOME/BugBounty/$TARGET/dir_lower_results.txt
     echo "Completed directory-list-lowercase-2.3-big.txt"
     
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Web-Content/raft-large-directories.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Web-Content/raft-large-directories.txt -o $HOME/BugBounty/$TARGET/raft_large_results.txt
     echo "Completed raft-large-directories.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Web-Content/common.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Web-Content/common.txt -o $HOME/BugBounty/$TARGET/web_content_results.txt
     echo "Complted common.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -o $HOME/BugBounty/$TARGET/top_million_results.txt
     echo "Completed subdomains-top1million-110000.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/DNS/namelist.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/DNS/namelist.txt -o $HOME/BugBounty/$TARGET/namelist_results.txt
     echo "Completed namelist.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Miscellaneous/us-cities.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Miscellaneous/us-cities.txt -o $HOME/BugBounty/$TARGET/cities_results.txt
     echo "Completed us-cities.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Miscellaneous/ike-groupid.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/SecLists/Discovery/Miscellaneous/ike-groupid.txt -o $HOME/BugBounty/$TARGET/ike_results.txt
     echo "Completed ike-groupid.txt"
 }
 
 dns_fuzzdb(){
-    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/dns/gTLD.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/dns/gTLD.txt -o $HOME/BugBounty/$TARGET/gtld_results.txt
     echo "Completed gTLD.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/dns/dnsmapCommonSubdomains.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/dns/dnsmapCommonSubdomains.txt -o $HOME/BugBounty/$TARGET/dnsmap_results.txt
     echo "Completed dnsmapCommonSubdomains.txt"
 
-    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/dns/alexaTop1mAXFRcommonSubdomains.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/dns/alexaTop1mAXFRcommonSubdomains.txt -o $HOME/BugBounty/$TARGET/alexa_top_results.txt
     echo "Completed alexaTop1mAXFRcommonSubdomains.txt" 
 
-    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/Miscellaneous/common-methods.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/Lists/fuzzdb/discovery/Miscellaneous/common-methods.txt -o $HOME/BugBounty/$TARGET/common_methods_results.txt
     echo "Completed common-methods.txt"
 
-    gobuster dns -d $TARGET -w ~/List/fuzzdb/wordlist-misc/wordlist-alphanumeric-case.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/List/fuzzdb/wordlist-misc/wordlist-alphanumeric-case.txt -o $HOME/BugBounty/$TARGET/alphanumeric_reults.txt
     echo "Completed wordlist-alphanumeric-case.txt"
 
-    gobuster dns -d $TARGET -w ~/List/fuzzdb/wordlist-user-passwd/names/namelist.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/List/fuzzdb/wordlist-user-passwd/names/namelist.txt -o $HOME/BugBounty/$TARGET/namelist_results2.txt
     echo "Completed namelist.txt"
 
-    gobuster dns -d $TARGET -w ~/List/fuzzdb/wordlist-user-passwd/unix-os/unix_users.txt -o $HOME/BugBounty/$TARGET/subdomains.txt
+    gobuster dns -d $TARGET -w ~/List/fuzzdb/wordlist-user-passwd/unix-os/unix_users.txt -o $HOME/BugBounty/$TARGET/unix_users_results.txt
     echo "Completed unix_users.txt"
 }
 
-ffuf(){
-    ffuf -w $PATHS_WORDLIST -u $TARGET/FUZZ >> $PATHS
-    ffuf -w $KEYS_WORDLIST -u $TARGET/FUZZ >> $PATHS
-}
-
-burl(){
-    cat $PATHS | burl | grep FORBIDDEN >> $FORBIDDEN_URLS
-    cat $PATHS | burl | grep grep -v "Not\|Forbidden" >> $UP_URLS
-}
-
-
-live_targets(){
-    cat $DIRTY_SUBDOMAINS | httprobe | unfurl --unique domains >> $LIVE_TARGETS
-}
-
 file_check
-custom_wordlist
 dns_commonspeak
-pause
 dns_seclist
-pause
 dns_fuzzdb
-ffuf
-live_targets
 echo "Completed Brute Force"
